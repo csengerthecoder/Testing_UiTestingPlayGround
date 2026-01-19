9a7b9a201ecef30932fdfff3ce0826bf3a9613b8
csengerthecoder <191019948+csengerthecoder@users.noreply.github.com>
2026-01-19 16:31:14 +0100

    mainPage & baseTest done

--- src/main/java/project/pages/MainPage.java
+++ src/main/java/project/pages/MainPage.java
@@ -1,4 +1,59 @@
 package project.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Objects;
+
 public class MainPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private final By dynamicButtonTaskButton = By.linkText("Dynamic ID");
+    private final By classAttributeButton = By.linkText("Class Attribute");
+    private final By hiddenLayersButton = By.linkText("Hidden Layers");
+    private final By loadDelayButton = By.linkText("Load Delay");
+    private final By ajaxDataButton = By.linkText("AJAX Data");
+    private final By scrollBars = By.linkText("Scrollbars");
+    private final By ProgressBarButton = By.linkText("Progress Bar");
+
+    public MainPage(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    private void click(By locator) {
+        Objects.requireNonNull(wait.until(ExpectedConditions.elementToBeClickable(locator))).click();
+    }
+
+//    public HiddenLayersPage openHiddenLayersPage() {
+//        click(hiddenLayersButton);
+//        return new HiddenLayersPage(driver, wait);
+//    }
+//    public ClassAttributePage openClassAttributePage() {
+//        click(classAttributeButton);
+//        return new ClassAttributePage(driver, wait);
+//    }
+//    public DynamicIdPage openDynamicIdPage() {
+//        click(dynamicButtonTaskButton);
+//        return new DynamicIdPage(driver, wait);
+//    }
+//    public LoadDelayPage openLoadDelayPage() {
+//        click(loadDelayButton);
+//        return new LoadDelayPage(driver, wait);
+//    }
+//    public AjaxDataPage openAjaxDataPage() {
+//        click(ajaxDataButton);
+//        return new AjaxDataPage(driver, wait);
+//    }
+//    public ScrollBarPage openScrollBarPage() {
+//        click(scrollBars);
+//        return new ScrollBarPage(driver, wait);
+//    }
+//    public ProgressBarPage openProgressBarPage() {
+//        click(ProgressBarButton);
+//        return new ProgressBarPage(driver, wait);
+//    }
 }
---
+++ src/test/java/tests/baseTest/BaseTest.java
@@ -0,0 +1,60 @@
+package tests.baseTest;
+
+import project.pages.MainPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected MainPage mainPage;
+
+    @BeforeEach
+    public void setUp() {
+        String gridUrl = System.getProperty("gridUrl");
+
+        ChromeOptions options = new ChromeOptions();
+
+        String osName = System.getProperty("os.name", "").toLowerCase();
+        String ci = System.getenv("CI");
+
+        if (osName.contains("linux") || (ci != null && ci.equalsIgnoreCase("true"))) {
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        if (gridUrl != null && !gridUrl.isBlank()) {
+            try {
+                URL url = new URL(gridUrl);
+                driver = new RemoteWebDriver(url, options);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Invalid gridUrl: " + gridUrl, e);
+            }
+        } else {
+            driver = new ChromeDriver(options);
+        }
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+        mainPage = new MainPage(driver, wait);
+        driver.get("http://uitestingplayground.com/home");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
